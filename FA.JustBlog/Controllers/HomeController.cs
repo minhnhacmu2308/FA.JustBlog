399f0dbc97f4ed05af313d3b2e43dc7721e0f06a
minhnhacmu2308 <65648352+minhnhacmu2308@users.noreply.github.com>
2021-12-11 02:24:42 +0700

    'new'

--- FA.JustBlog/Controllers/HomeController.cs
+++ FA.JustBlog/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fa.JustBlog.Daos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,23 @@ namespace FA.JustBlog.Controllers
 {
     public class HomeController : Controller
     {
+        PostDao postDao = new PostDao();
         public ActionResult Index()
         {
+            ViewBag.list = postDao.getPost();
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            ViewBag.post = postDao.getPostById(id);
+            return View();
+        }
+
+
+        public ActionResult ListPost()
+        {
+            ViewBag.list = postDao.getAll();
             return View();
         }
 
--- FA.JustBlog/Daos/PostDao.cs
+++ FA.JustBlog/Daos/PostDao.cs
@@ -16,6 +16,16 @@ namespace Fa.JustBlog.Daos
             return myDb.Posts.OrderByDescending(p => p.id_post).ToList();
         }
 
+        public List<Post> getPost()
+        {
+            return myDb.Posts.OrderByDescending(p => p.id_post).Take(5).ToList();
+        }
+
+
+        public Post getPostById(int id)
+        {
+            return myDb.Posts.FirstOrDefault(x =>x.id_post == id);
+        }
         public void addPost(Post post)
         {
             myDb.Posts.Add(post);
